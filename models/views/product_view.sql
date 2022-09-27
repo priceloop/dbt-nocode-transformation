58660af77b13ecd4af45d8d4ec38ba0108824e62
bayoog <obaydaba96@gmail.com>
2022-09-27 10:20:29 +0300

    feat: remove views

--- models/views/product_view.sql
+++
@@ -1,7 +0,0 @@
-{{ config(alias=env_var('DESTINATION_TABLE', var('destination_table', '')) + "_view") }}
-
-with view as (
-    select * from {{ ref('product') }}
-)
-
-select * from view
--- models/views/product_view_request.sql
+++
@@ -1,7 +0,0 @@
-{{ config(alias=env_var('DESTINATION_TABLE', var('destination_table', '')) + "_view_request") }}
-
-with request as (
-    select * from {{ ref('product') }}
-)
-
-select * from request
